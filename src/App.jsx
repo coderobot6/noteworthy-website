0b6fa554cb5774e7d4059c64a613791f0c52ddf8
kai tran <ultra1524@gmail.com>
2024-03-17 20:33:54 -0700

    add landingpage and about us description

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import teamPhoto from "./assets/noteworthy-group.png";
+import "./App.css";
+import noteWorthyTitle from "./assets/noteworthy-title.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="scroll-container">
+      <div
+        id="landing-page"
+        className="parallax"
+        style={{
+          backgroundImage: `url(${teamPhoto})`,
+        }}
+      >
+        <img
+          src={noteWorthyTitle}
+          alt="Noteworthy A Cappella"
+          style={{ marginBottom: "7%", marginLeft: "3%" }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div id="about-us">
+        <h1 id="about-us-title">NOTEWORTHY</h1>
+        <h1 id="about-us-subtitle">In-vest-ed in A Cappella</h1>
+
+        <h3 id="about-us-description1">
+          We’re a lower-voices a cappella group known for our fun performances
+          and fresh takes on popular songs! It always feels <mark>sunny</mark> when we
+          perform, and not just because of our <mark>golden vests</mark>!
+        </h3>
+
+        <h3 id="about-us-description2">
+          Since our founding in 2000, we’ve gone million-view-viral on YouTube
+          multiple times, released an album entirely composed of original songs,
+          and performed at the 2022 UC Berkeley commencement.
+        </h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
